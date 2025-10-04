524099bb7f2dd57f7dfd0ffddfc9d022f9bf3ace
Bupe Katongo <107362236+6upe@users.noreply.github.com>
2025-10-04 02:56:49 -0700

    Update 20250924060522_add_stripe_fields_to_subscriptions.js

--- migrations/20250924060522_add_stripe_fields_to_subscriptions.js
+++ migrations/20250924060522_add_stripe_fields_to_subscriptions.js
@@ -1,13 +1,11 @@
 exports.up = async function(knex) {
   await knex.schema.alterTable('subscriptions', table => {
-    table.string('stripe_checkout_session_id').nullable();
     table.string('stripe_subscription_id').nullable();
   });
 };
 
 exports.down = async function(knex) {
   await knex.schema.alterTable('subscriptions', table => {
-    table.dropColumn('stripe_checkout_session_id');
     table.dropColumn('stripe_subscription_id');
   });
 };
